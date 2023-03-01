8e93bd7c0bbff39ed998f5c80a85dd3153c0f0d3
sergio <apontelamusergioandres@gmail.com>
2023-03-01 13:57:39 -0500

    inicializacion

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
-let number=0
-let number2=0
-function mostrar(){
-    number2=document.getElementById('numeros')
-    resultado= parseInt(number) + parseInt(number2)
-    document.getElementById("display").innerText=resultado
+
+for (let i=0; i<=9; i++){
+    document.getElementById('btn_' + i).addEventListener('click', (evento)=>mostrar(evento.target));
 }
-function operador(operando){
-    switch(operando){
-            case '+':
-                simbolo.innerText='+'
-                number=document.getElementById('numeros')
-                numeros.value=''
-                break
-            case '-':
-                resultado=parseInt(num1.value) - parseInt(num2.value)
-                simbolo.innerText='-'
-                console.log(resultado)
-                break
-            case '*':
-                resultado=parseInt(num1.value) * parseInt(num2.value)
-                simbolo.innerText='*'
-                console.log(resultado)
-                break
-            case '/':
-                resultado=parseInt(num1.value) / parseInt(num2.value)
-                simbolo.innerText='/'
-                console.log(resultado)
-                break
-            case 'AC':
-                resultado=' '
-                simbolo.innerText=' '
-                num1.value=' '
-                num2.value=' '
-                mostrar()
-                break
+function mostrar(tecla){
+    document.getElementById('display').innerHTML=
+    document.getElementById('display').innerHTML+tecla.value
+}
+function mostrar2(tecla){
+    document.getElementById('display').innerHTML=
+    document.getElementById('display').innerHTML+tecla.value
+}
+addEventListener('keypress', evento=>{
+    if(!isNaN(evento.key)){
+        mostrar2(evento.key)
+    }
+})
+
+function valor(ingresar){
+    let num=ingresar
+    switch (num.type){
+        case '1':
+            
     }
 }
